2e24657725c5470608c1293522fc465c5f4e0b15
Hp <Hp@Hp15Ryzen7000>
2025-03-31 17:17:17 -0300

    primer proyecto en blanco

---
+++ EJEMPLO1/src/ejemplo1/EJEMPLO1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo1;
+
+/**
+ *
+ * @author Hp
+ */
+public class EJEMPLO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
