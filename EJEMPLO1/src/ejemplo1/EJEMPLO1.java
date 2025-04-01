f38d8dbaaa410087f1d4fe6b6a0be2bd47c40e0d
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-04-01 00:57:48 -0300

    Asignacion de las variables de entradas, creacion de un array de tonos

--- EJEMPLO1/src/ejemplo1/EJEMPLO1.java
+++ EJEMPLO1/src/ejemplo1/EJEMPLO1.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejemplo1;
-
-/**
- *
- * @author Alzab
- */
+import java.util.*;
 public class EJEMPLO1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
+        String n = scanner.next();
+        String tonos[] = {"DO","DO#","RE","RE#","MI","FA","FA#","SOL","SOL#","LA","LA#","SI"};
+       
     }
-    
 }
