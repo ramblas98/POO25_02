06314ee941aa04d8ecfaa85a9d4c9f944fc9befa
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-04-01 01:41:49 -0300

    Ejercicio terminado

--- EJEMPLO1/src/ejemplo1/EJEMPLO1.java
+++ EJEMPLO1/src/ejemplo1/EJEMPLO1.java
@@ -5,7 +5,31 @@ public class EJEMPLO1 {
         Scanner scanner = new Scanner(System.in);
         int s = scanner.nextInt();
         String n = scanner.next();
+        n = n.toUpperCase();
         String tonos[] = {"DO","DO#","RE","RE#","MI","FA","FA#","SOL","SOL#","LA","LA#","SI"};
-       
+        int tamanio = tonos.length;
+        /*
+        Se busca el indice de la nota en el array
+        */
+        int index = -1;
+        boolean bandera = false;
+        int i = 0;
+        while(i<tamanio && bandera == false){
+            if(tonos[i].equals(n)){
+                index = i;
+                bandera = true;
+            }else{
+                i++;
+            }
+        }
+        /*
+        Se hace el siguiente calculo: restando el indice por el entero de entrada
+        luego se suma el resultado de la resta por el tamaño del array
+        para evitar numeros negativos, por ultimo se toma el resultado de esa operacion 
+        y se lo divide con el tamaño del array mediante mod, para asi se mantenga
+        en el rango de 0 a 11
+        */
+        int indexAux = (index - s  +tamanio) % tamanio;
+        System.out.println(tonos[indexAux]);
     }
 }
