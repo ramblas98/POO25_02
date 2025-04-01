b98129ef250f1c18865cd206a10a24efaee5eaec
nicor <nicor@DESKTOP-A2HO0PS>
2025-04-01 01:04:39 -0300

    PrimeraVersion

--- Ejemplo1/src/ejemplo1/Ejemplo1.java
+++ Ejemplo1/src/ejemplo1/Ejemplo1.java
@@ -4,6 +4,8 @@
  */
 package ejemplo1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nicor
@@ -15,6 +17,31 @@ public class Ejemplo1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        
+        String n;
+        String[] notas={"do", "do#", "re", "re#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si"};
+        int s, i, pos=-1;
+        
+        s = teclado.nextInt();
+        teclado.nextLine();
+        n = teclado.nextLine();
+        
+        n = n.toLowerCase();
+        
+        for(i=0; i<notas.length; i++){
+            if(notas[i].equals(n)){
+                pos = i;
+            }
+        }
+        
+        if(s>=1 && s<=3){       
+          if(pos >= s){
+              System.out.println(notas[pos-s]);
+          }else{
+              System.out.println(notas[notas.length+(pos-s)]);
+          }
+        }       
     }
     
 }
