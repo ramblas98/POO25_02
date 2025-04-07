8d6d6b0e1fd487f0964d28fd8da034fb4a6e48b8
Administrador <Administrador@ExpeUEW7>
2025-04-07 16:40:22 -0300

    primera version de Fecha

---
+++ TP2_EJERCICIO1/src/tp2_ejercicio1/Fecha.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2_ejercicio1;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    public Fecha() {
+    }
+
+    public Fecha(int dia, int mes, int anio) {
+        this.dia = dia;  // el + significa publico y el - significa privado
+        this.mes = mes; //this significa este o esto
+        this.anio = anio;
+    }
+    //GETTERS
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+    
+    //SETTERS
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
+}
