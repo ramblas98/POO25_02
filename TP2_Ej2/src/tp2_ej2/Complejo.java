28422d121a945c13220ad293c6460973a9ed685a
Hp <Hp@Hp15Ryzen7000>
2025-04-07 17:14:43 -0300

    Primera version de Complejo

---
+++ TP2_Ej2/src/tp2_ej2/Complejo.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_ej2;
+
+/**
+ *
+ * @author Hp
+ */
+public class Complejo {
+    private double re:
+    private double im;
+
+    public Complejo() {
+    }
+
+    public Complejo(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public void setRe(double re) {
+        this.re = re;
+    }
+
+    public void setIm(double im) {
+        this.im = im;
+    }
+    
+    
+}
