e21939338d4eddd03bc3c85a5302dc9a8c166dc5
profeblas <profeblas@galactica25>
2025-04-07 16:40:20 -0300

    primera version de Fecha

---
+++ TP2_EJ1/src/tp2_ej1/Fecha.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_ej1;
+
+/**
+ *
+ * @author profeblas
+ */
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    public Fecha() {
+    }
+
+    public Fecha(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+    
+    
+    
+}
