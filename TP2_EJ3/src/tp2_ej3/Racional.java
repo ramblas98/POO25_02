d539a3509aafecce8ab4e86e03ad7cee9887673b
alexj <alexj@ITS_Axello>
2025-04-07 16:40:31 -0300

    Primera Version de Racional

---
+++ TP2_EJ3/src/tp2_ej3/Racional.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_ej3;
+
+/**
+ *
+ * @author alexj
+ */
+public class Racional {
+    
+    private int num;
+    private int den;
+
+    public Racional() {
+        
+        
+        
+    }
+
+    public Racional(int num, int den) {
+        
+        this.num = num;
+        this.den = den;
+        
+    }
+
+    public int getNum() {
+        
+        return num;
+        
+    }
+
+    public int getDen() {
+        
+        return den;
+        
+    }
+
+    public void setNum(int num) {
+        
+        this.num = num;
+        
+    }
+
+    public void setDen(int den) {
+        
+        this.den = den;
+        
+    }
+    
+    
+    
+}
---
+++ TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_ej3;
+
+/**
+ *
+ * @author alexj
+ */
+public class TP2_EJ3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
