5d8c91912205c648ede7b19cbad4f88af77b5e7d
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-04-08 20:46:48 -0300

    Metodos y metodos estaticos agregados a la clase

--- TP2_EJ2/src/tp2_ej2/Complejo.java
+++ TP2_EJ2/src/tp2_ej2/Complejo.java
@@ -5,6 +5,9 @@ public class Complejo {
     private double real;
     private double imaginario;
 
+    /*
+     * Constructor
+     */
     public Complejo() {
     }
     
@@ -12,7 +15,9 @@ public class Complejo {
         this.real = real;
         this.imaginario = imaginario;
     }
-
+    /*
+     * GETTERS
+     */
     public double getReal() {
         return real;
     }
@@ -20,7 +25,9 @@ public class Complejo {
     public double getImaginario(){
         return imaginario;
     }
-
+    /*
+     * SETTERS
+     */
     public void setReal(double real) {
         this.real = real;
     }
@@ -28,5 +35,98 @@ public class Complejo {
     public void setImaginario(double imaginario){
         this.imaginario = imaginario;
     }
+
+
+    /*
+     * METODOS ESTATICOS
+     */
+
+     public static Complejo suma(Complejo z1, Complejo z2){
+        double parteReal = z1.real + z2.real;
+        double parteImaginaria = z1.imaginario + z2.imaginario;
+        Complejo resultado = new Complejo();
+        resultado.setImaginario(parteImaginaria);
+        resultado.setReal(parteReal);
+        return resultado;
+    }
+
+    public static Complejo resta(Complejo z1, Complejo z2){
+        double parteReal = z1.real - z2.real;
+        double parteImaginaria = z1.imaginario - z2.imaginario;
+        Complejo resultado = new Complejo(parteReal,parteImaginaria);
+        return resultado;
+    }
+
+    public static Complejo producto(Complejo z1, Complejo z2){
+        double parteReal = (z1.real*z2.real) - (z1.imaginario*z2.imaginario);
+        double parteImaginario = (z1.real * z2.imaginario) + (z1.imaginario * z2.real);
+        Complejo resultado = new Complejo(parteReal,parteImaginario);
+        return resultado;
+    }
+
+    public static Complejo division(Complejo z1, Complejo z2){
+        double a = z1.real;
+        double b = z1.imaginario;
+        double c = z2.real;
+        double d = z2.imaginario;
+        double denominador = c*c + d*d;
+        double parteReal = (a*c + b*d)/denominador;
+        double parteImaginaria = (b*c - a*d)/denominador;
+        return new Complejo(parteReal,parteImaginaria);
+    }
+
+    /*
+     * METODOS
+     */
+
+    @Override
+    public String toString() {
+        return "(" + real + "," + imaginario + "i)";
+    }
     
+    public Complejo suma(Complejo otro){
+        double parteReal = real + otro.real;
+        double parteImaginaria = imaginario + otro.imaginario;
+        return new Complejo(parteReal, parteImaginaria);
+        
+    }
+    
+    public Complejo resta(Complejo otro){
+        double parteReal = otro.real - real;
+        double parteImaginaria = otro.imaginario - imaginario;
+        return new Complejo(parteReal,parteImaginaria);
+    }
+    
+    public Complejo producto(Complejo otro){
+        double parteReal = ((otro.real*real) - (otro.imaginario*imaginario));
+        double parteImaginaria =  (otro.real * imaginario)  +  (otro.imaginario * real);
+        return new Complejo(parteReal, parteImaginaria);
+    }
+    
+    public Complejo dividir(Complejo otro) {
+        double a = this.real;
+        double b = this.imaginario;
+        double c = otro.real;
+        double d = otro.imaginario;
+
+        double denominador = c * c + d * d;
+
+        double parteReal = (a * c + b * d) / denominador;
+        double parteImaginaria = (b * c - a * d) / denominador;
+
+        return new Complejo(parteReal, parteImaginaria);
+    }
+
+    public Complejo Conjugado(){
+        return new Complejo(real,-imaginario);
+    }    
+    public double Modulo(){
+        double modulo = Math.sqrt(Math.pow(real, 2) + Math.pow(imaginario, 2));
+        return modulo;
+    }
+    
+    public boolean equals(Object obj){
+        
+        return true;
+    }
 }
