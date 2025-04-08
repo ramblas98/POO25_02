9198068593e89b19f6b736944f1a37a7cfb0b4ba
IvanGonzalo23 <alzabet-ivang@hotmail.com>
2025-04-08 20:47:12 -0300

    Prueba de los metodos agregados

--- TP2_EJ2/src/tp2_ej2/TP2_EJ2.java
+++ TP2_EJ2/src/tp2_ej2/TP2_EJ2.java
@@ -1,10 +1,14 @@
 package tp2_ej2;
 public class TP2_EJ2 {
     public static void main(String[] args) {
-      Complejo c1 = new Complejo();
-      c1.setReal(2);
-      c1.setImaginario(1);
-      System.out.print("(" + c1.getReal() + "," + c1.getImaginario() + "i)");
+      // Complejo c1 = new Complejo();
+      // c1.setReal(2);
+      // c1.setImaginario(1);
+      // System.out.print("(" + c1.getReal() + "," + c1.getImaginario() + "i)");
+      Complejo z1 = new Complejo(3,4);
+      // System.out.println(z1.Modulo());
+
+      System.out.print(z1.Conjugado());
     }
     
 }
