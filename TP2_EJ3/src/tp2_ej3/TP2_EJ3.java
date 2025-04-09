31d2a21d7e5b0f1645ea02de51140b8ec3147bf4
HP ZBOOK G5 <HP ZBOOK G5@DESKTOP-FR7ULDO.mshome.net>
2025-04-09 00:30:31 -0300

    PRIMERA

--- TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
+++ TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
@@ -14,7 +14,34 @@ public class TP2_EJ3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Complejo x = new Complejo();
+        x.setReal(3);
+        x.setImaginaria(2);
+        System.out.println(x);
+        Complejo y = new Complejo();
+        y.setReal(1);
+        y.setImaginaria(-4);
+        System.out.println(y);
+        
+        Complejo z;
+        z = x.sumar(y);
+        System.out.println("La suma de dos complejos es: "+z);
+        
+        z = x.restar(y);
+        System.out.println("La suma de dos complejos es: "+z);
+        
+        z = x.multiplicar(y);
+        System.out.println("El producto de dos complejos es: "+z);
+        
+        z = x.division(y);
+        System.out.println("La division de dos complejos es: "+z);
+        
+        System.out.println("El modulo de dos complejos es: "+x.modulo());
+        
+        System.out.println("El argumento de x: "+x.argumento());
+        
+        z = x.conjugado();
+        System.out.println("El producto de dos complejos es: "+z);
     }
     
 }
