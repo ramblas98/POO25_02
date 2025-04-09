0344bd2796b6eb01c99fe85c4f4c1065ccae255f
leona <leona@REIMAN>
2025-04-09 17:23:51 -0300

    primera

---
+++ TP2_EJ3/src/tp2_ej3/Racional.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_ej3;
+
+/**
+ *
+ * @author leona
+ */
+public class Racional {
+  private int numerador;
+  private int denominador;
+
+    public Racional() {
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+   
+    public 
+  
+
--- TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
+++ TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
@@ -15,6 +15,10 @@ public class TP2_EJ3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        
     }
     
 }
