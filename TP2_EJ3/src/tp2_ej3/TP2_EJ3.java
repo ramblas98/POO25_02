84f7038e65b8870c00dcd7354b910929c045a293
AlexMartin1427 <116049140+AlexMartin1427@users.noreply.github.com>
2025-04-12 12:59:12 -0300

    Update TP2_EJ3.java

--- TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
+++ TP2_EJ3/src/tp2_ej3/TP2_EJ3.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tp2_ej3;
 
-/**
- *
- * @author HP ZBOOK G5
- */
 public class TP2_EJ3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Complejo x = new Complejo();
         x.setReal(3);
