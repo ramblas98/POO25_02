7867caaf5431ce2502aabd0b687e0442e3488560
HP ZBOOK G5 <HP ZBOOK G5@DESKTOP-FR7ULDO.mshome.net>
2025-04-12 12:26:05 -0300

    PRIMERA

--- TP2_EJ3/src/tp2_ej3/Complejo.java
+++ TP2_EJ3/src/tp2_ej3/Complejo.java
@@ -28,16 +28,7 @@ public class Complejo {
 
     public void setImaginaria(double imaginaria) {
         this.imaginaria = imaginaria;
-    }
-
-    @Override
-    public String toString() {
-        if(imaginaria>=0){
-            return real + "+" + imaginaria + "i" ;
-        }else{
-            return real + "" + imaginaria + "i" ;
-        }
-    }
+    } 
     
     public Complejo sumar(Complejo otro){
         Complejo x = new Complejo();
@@ -79,8 +70,67 @@ public class Complejo {
      public Complejo conjugado(){
         return new Complejo(this.real,-this.imaginaria);
     }
-     
-     public boolean equals(){
-         
-     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.real) ^ (Double.doubleToLongBits(this.real) >>> 32));
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.imaginaria) ^ (Double.doubleToLongBits(this.imaginaria) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Complejo other = (Complejo) obj;
+        if (Double.doubleToLongBits(this.real) != Double.doubleToLongBits(other.real)) {
+            return false;
+        }
+        return Double.doubleToLongBits(this.imaginaria) == Double.doubleToLongBits(other.imaginaria);
+    }
+    @Override
+    public String toString() {
+        if(imaginaria>=0){
+            return real + "+" + imaginaria + "i" ;
+        }else{
+            return real + "" + imaginaria + "i" ;
+        }
+    }
+    
+    public static Complejo sumar(Complejo c1, Complejo c2){
+        Complejo x = new Complejo();
+        x.real = c1.real+c2.real;
+        x.imaginaria = c1.imaginaria+c2.imaginaria;
+        return x;
+    }
+    
+    public static Complejo restar(Complejo c1, Complejo c2){
+        Complejo x = new Complejo();
+        x.real = c1.real - c2.real;
+        x.imaginaria = c1.imaginaria - c2.imaginaria;
+        return x;
+    }
+    
+    public static Complejo multiplicar(Complejo c1, Complejo c2){
+        Complejo x = new Complejo();
+        x.real = c2.real*c1.real-c2.imaginaria*c1.imaginaria;
+        x.imaginaria = c2.real*c1.imaginaria+c2.imaginaria*c1.real;
+        return x;
+    }
+    
+    public static Complejo division(Complejo c1, Complejo c2){
+        double denominador = Math.pow(c2.real, 2)+Math.pow(c2.imaginaria, 2);
+        Complejo x = new Complejo();
+        x.real = c2.real/denominador;
+        x.imaginaria = -c2.imaginaria/denominador;
+        return x.multiplicar(c1);
+    }
 }
