750a0a431f36da5f4a9d9ee930acca2cdabe91ef
nicor <nicor@DESKTOP-A2HO0PS>
2025-04-14 15:59:44 -0300

    PrimeraVersion

---
+++ TP2_EJ5/src/tp2_ej5/Estudiante.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_ej5;
+
+/**
+ *
+ * @author nicor
+ */
+public class Estudiante {
+    private String apellido;
+    private String nombre;
+    private int lu;
+    private String email;
+    public boolean regular;
+    private int notaP1;
+    private int notaP2;
+    private int notaR;
+    private int asistencia;
+}
