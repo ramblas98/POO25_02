f963c4e211c9bc54cb692ee9bbd6925a9fefc87a
Blade <Blade@LAPTOP-O169HTE7>
2025-04-14 15:28:34 -0300

    v1.0

---
+++ TP2_Ej5/src/tp2_ej5/Estudiante.java
@@ -0,0 +1,15 @@
+package tp2_ej5;
+
+public class Estudiante {
+    private String apellidos;
+    private String nombres;
+    private int lu;
+    private String email;
+    public boolean regular;
+    private int notaP1;
+    private int notaP2;
+    private int notaR;
+    private int asistencia;
+}
+
+
---
+++ TP2_Ej5/src/tp2_ej5/TP2_Ej5.java
@@ -0,0 +1,8 @@
+package tp2_ej5;
+
+public class TP2_Ej5 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
