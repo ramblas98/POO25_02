6dfb9647a38ea52e738c1c89676433759a00129f
nicor <nicor@DESKTOP-A2HO0PS>
2025-04-14 15:59:55 -0300

    PrimeraVersion

---
+++ TP2_EJ5/src/tp2_ej5/TP2_EJ5.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_ej5;
+
+/**
+ *
+ * @author nicor
+ */
+public class TP2_EJ5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Estudiante lista[] = new Estudiante[100];
+        int opc, i, n;
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        n = 0;
+        opc = 1;
+        
+        while(opc!=0){
+            lista[n] = Estudiante.nuevo();
+            n++;
+            
+            System.out.println("1- Ingreasr otro estudiante");
+            System.out.println("0- Salir");
+            
+            opc = teclado.nextInt();
+        }
+        
+        System.out.println("Estudiantes regulares:");
+        for(i=0; i<n; i++){
+          if(lista[i].regular()){
+              System.out.println(lista[i]);
+          }
+        }
+    }
+    
+}
