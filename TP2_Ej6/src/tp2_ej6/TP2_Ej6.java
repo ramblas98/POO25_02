782b1f34a0de80ab9d88c8850c074715f4900f0e
Blade <Blade@LAPTOP-O169HTE7>
2025-04-23 16:46:55 -0300

    v1.1

--- TP2_Ej6/src/tp2_ej6/TP2_Ej6.java
+++ TP2_Ej6/src/tp2_ej6/TP2_Ej6.java
@@ -3,7 +3,25 @@ package tp2_ej6;
 public class TP2_Ej6 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Carga por constructor
+        Vendedor v1 = new Vendedor("Lopez","Blas",22222222,1,"Oran");
+        Vendedor v2 = new Vendedor("Reiman","Leo",11111111,2,"Pichanal");
+        Vendedor v3 = new Vendedor("Torres","Juan",33333333,3,"Urundel");
+        
+        //Carga por teclado
+      /*Vendedor v1 = new Vendedor();
+        Vendedor v2 = new Vendedor();
+        Vendedor v3 = new Vendedor();
+        
+        v1 = v1.cargarDatos();
+        v2 = v2.cargarDatos();
+        v3 = v3.cargarDatos();*/
+        
+        v1.generarVentas();
+        v2.generarVentas();
+        v3.generarVentas();
+        
+        System.out.println("Comisiones \n\nVendedor 1: " + v1.comision() + "\nVendedor 2: " + v2.comision() + "\nVendedor 3: " + v3.comision());
     }
     
 }
--- TP2_Ej6/src/tp2_ej6/Vendedor.java
+++ TP2_Ej6/src/tp2_ej6/Vendedor.java
@@ -1,10 +1,15 @@
 package tp2_ej6;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Vendedor extends Persona{
     private Persona datosPersonales;
     private int legajo;
     private String zonaDeVenta;
     private float ventasMensuales[];
+    
+    Scanner teclado = new Scanner(System.in);
 
     public Vendedor() {
     }
@@ -13,9 +18,86 @@ public class Vendedor extends Persona{
         this.datosPersonales = datosPersonales;
         this.legajo = legajo;
         this.zonaDeVenta = zonaDeVenta;
+        this.ventasMensuales = new float[12];
+        for(int i=0;i<12;i++) this.ventasMensuales[i] = 0;
+    }
+
+    public Vendedor(String apellido, String nombre, int dni, int legajo, String zonaDeVenta) {
+        this.datosPersonales = new Persona(apellido,nombre,dni);
+        this.legajo = legajo;
+        this.zonaDeVenta = zonaDeVenta;
+        this.ventasMensuales = new float[12];
         for(int i=0;i<12;i++) this.ventasMensuales[i] = 0;
     }
     
+
+    public Persona getDatosPersonales() {
+        return datosPersonales;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public String getZonaDeVenta() {
+        return zonaDeVenta;
+    }
+
+    public void setDatosPersonales(Persona datosPersonales) {
+        this.datosPersonales = datosPersonales;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public void setZonaDeVenta(String zonaDeVenta) {
+        this.zonaDeVenta = zonaDeVenta;
+    }
     
+    
+    public void generarVentas(){
+        Random random = new Random();
+        for(int i=0;i<12;i++) this.ventasMensuales[i] = (float) (random.nextInt(10000 - 2000) + 2000);
+    }
+    
+    public float totalAnual(){
+        float total = 0;
+        for(int i=0;i<12;i++) total += this.ventasMensuales[i];
+        return total;
+    }
+    
+    public float comision(){
+        float total = this.totalAnual();
+        if(total>=50000){
+            if(total<=75000) return (float) total/100 * 15;
+            else{
+                if(total<=100000) return (float) total/100 * 20;
+                else return (float) total/100 * 30;
+            }
+        }
+        else return 0;
+    }
+    
+    public void agregarVenta(int mes,float venta){
+        this.ventasMensuales[mes-1] = venta;
+    }
+    
+    public Vendedor cargarDatos(){
+        Persona datos = new Persona();
+        System.out.println("Ingrese los datos del Vendedor\n");
+        System.out.println("Ingrese nombre: ");
+        datos.setNombre(teclado.next());
+        System.out.println("Ingrese apellido: ");
+        datos.setApellido(teclado.next());
+        System.out.println("Ingrese dni: ");
+        datos.setDni(teclado.nextInt());
+        System.out.println("Ingrese legajo: ");
+        int leg = teclado.nextInt();
+        System.out.println("Ingrese zona de venta: ");
+        String zv = teclado.next();
+        
+        return new Vendedor(datos,leg,zv);
+    }
 }
 
