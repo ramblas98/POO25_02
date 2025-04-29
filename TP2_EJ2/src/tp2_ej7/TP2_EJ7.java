96d34c0772c26112720f7bfb67f65267061c7f08
IvanGonzalo23 <134026292+IvanGonzalo23@users.noreply.github.com>
2025-04-28 22:24:48 -0300

    Update TP2_EJ7.java

--- TP2_EJ2/src/tp2_ej7/TP2_EJ7.java
+++ TP2_EJ2/src/tp2_ej7/TP2_EJ7.java
@@ -1,38 +1,42 @@
 package tp2_ej7;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TP2_EJ7 {
+    public static boolean asignarMedicoHistoria(Historia h, Medico m){
+        if( ( h.getEspecialidad() == m.getEspecialidad() ) && m.disponibilidad()){
+            m.agregarHistoria(h);
+            h.setMedicoAsignado(m);
+            return true;
+        }else{
+            return false;
+        }
+    }
     public static void main(String[] args) {
-        Medico m1 = new Medico("Nortes",5677,"pediatria",5);
-        Medico m2 = new Medico("Gutierrez", 4568);
-        Medico m3 = new Medico("Mayor", 7890);
-        Medico m4 = new Medico("Guillen", 4567, "traumatologia", 5);
-        Medico m5 = new Medico("Mateo", 8976, "traumatologia", 5);
+        List <Historia> misPacientes = new ArrayList<>();
+    
+        CuadroMedico misDoc = new CuadroMedico();
+        for (int i = 0; i < misPacientes.size(); i++) {
 
-        CuadroMedico cMedico = new CuadroMedico();
-        cMedico.insertarMedico(m1);
-        cMedico.insertarMedico(m2);
-        cMedico.insertarMedico(m3);
-        cMedico.insertarMedico(m4);
-        cMedico.insertarMedico(m5);
-
-        Historia historias[] = new Historia[5];
-        historias[0] = new Historia("Rocio", "Leve", "pediatria");
-        historias[1] = new Historia("Andres", "grave", "traumatologia");
-        historias[2] = new Historia("Juan", "medio", "traumatologia");
-        historias[3] = new Historia("Marieta", "leve", "familia");
-        historias[4] = new Historia("Alfonso", "medio", "familia");
-
-        for (Historia h : historias) {
-            String especialidad = h.getEspecialidad();
-            Medico medico = cMedico.obtenerMedicoDisponible(especialidad);
-        
-            if (medico != null) {
-                medico.agregarHistoria(h);
-                h.setMedicoAsignado(medico);
-                System.out.println(medico.getNombre() + " - " + h.getCP() + " - " + h.getId());
-            } else {
-                System.out.println("No hay médico disponible para " + h.getCP() + " (" + especialidad + ")");
+            Medico m = misDoc.obtenerMedicoMasDisponible(misPacientes.get(i).getEspecialidad());
+            if(m != null){
+                boolean b = asignarMedicoHistoria(misPacientes.get(i), m);
+                if(b){
+                    System.out.println(  m.getNombre() + "-" +"-" + "");
+                }
             }
         }
-        
+        for (int i = 0; i < misPacientes.size(); i++) {
+            Medico m = misPacientes.get(i).getMedico();
+            if(misPacientes.get(i).getMedico() != null){
+              
+                  misPacientes.get(i).setAlta(true);
+                  m.darAlta(misPacientes.get(i).getId());
+                 
+                
+                 
+
+            }
+        }
     }
 }
